51650e4527546a3239484ed8d2c40c330db94a4d
Jonas <jag@agertoft.net>
2017-06-15 14:14:30 +0100

    test

--- CITest/Controllers/HomeController.cs
+++ CITest/Controllers/HomeController.cs
@@ -16,7 +16,7 @@ namespace CITest.Controllers
 
         public ActionResult About()
         {
-            ViewBag.Message = "Your application description page.";
+            ViewBag.Message = "Your application description test page.";
 
             return View();
         }
